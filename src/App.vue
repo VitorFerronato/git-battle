ee2f06499a6546e35df5d591363f8a87f39c07c9
Vitor Eduardo Ferronato <vitor.ferronato@neogrid.com>
2025-03-14 23:32:21 -0300

    feat: initialize project

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <div class="image-box">
+      <img src="./assets/wallpapper.png" alt="" />
+    </div>
+
+    <div class="input-container">
+      <div class="input-box">
+        <GBInput label="User 1" />
+        <span>VS</span>
+        <GBInput label="User 2" />
+      </div>
+    </div>
+
+    <div class="button-box">
+      <GBButton />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import GBInput from "./components/GBInput.vue";
+import GBButton from "./components/GBButton.vue";
+</script>
+
+<style scoped>
+#app {
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--bg-color);
+}
+
+.image-box {
+  display: flex;
+  justify-content: center;
+  padding-top: 2rem;
+}
+
+.image-box img {
+  width: 300px;
+}
+
+.input-container {
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  margin-top: 2rem;
+}
+
+.input-container .input-box {
+  display: flex;
+  align-items: center;
+  gap: 4rem;
+}
+
+.input-box span {
+  color: var(--white-color);
+  margin-top: 3rem;
+}
+
+.button-box {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+}
+</style>
---
+++ src/components/GBButton.vue
@@ -0,0 +1,133 @@
+<template>
+  <a href="#" class="button">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    Fight!</a
+  >
+</template>
+
+<style scoped>
+a {
+  position: absolute;
+  padding: 12px 24px;
+  color: var(--primary-color);
+  text-transform: uppercase;
+  text-decoration: none;
+  letter-spacing: 5px;
+  font-size: 16px;
+  box-shadow: 0 20px 50px rgba(0, 0, 0, 0.5);
+  overflow: hidden;
+  transition: 0.2s;
+}
+
+a:hover {
+  transform: scale(1.1);
+}
+
+a::before {
+  content: "";
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  bottom: 2px;
+  width: 50%;
+}
+
+a span:nth-child(1) {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(
+    to right,
+    var(--secondary-color),
+    var(--primary-color)
+  );
+  animation: animate1 2s linear infinite;
+  animation-delay: 1s;
+}
+
+@keyframes animate1 {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(100%);
+  }
+}
+
+a span:nth-child(2) {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    var(--secondary-color),
+    var(--primary-color)
+  );
+  animation: animate2 2s linear infinite;
+  animation-delay: 2s;
+}
+
+@keyframes animate2 {
+  0% {
+    transform: translateY(-100%);
+  }
+  100% {
+    transform: translateY(100%);
+  }
+}
+
+a span:nth-child(3) {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(
+    to left,
+    var(--secondary-color),
+    var(--primary-color)
+  );
+  animation: animate3 2s linear infinite;
+  animation-delay: 1s;
+}
+
+@keyframes animate3 {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+
+a span:nth-child(4) {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(
+    to top,
+    var(--secondary-color),
+    var(--primary-color)
+  );
+  animation: animate4 2s linear infinite;
+  animation-delay: 2s;
+}
+
+@keyframes animate4 {
+  0% {
+    transform: translateY(100%);
+  }
+  100% {
+    transform: translateY(-100%);
+  }
+}
+</style>
