d843fc1c22fe5dfa1fca5fa9d2d8f520be55b067
Vitor Eduardo Ferronato <vitor.ferronato@neogrid.com>
2025-03-15 15:23:31 -0300

    feat: add transitions

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-    <BattleInputs v-if="!results" @update:results="results = $event" />
-
-    <BattleCard v-else :results="results" @resetBattle="results = null" />
+    <Transition name="slide" mode="out-in">
+      <BattleInputs v-if="!results" @update:results="results = $event" />
+      <BattleCard v-else :results="results" @resetBattle="results = null" />
+    </Transition>
   </div>
 </template>
 
@@ -16,6 +17,21 @@ const results = ref(null);
 </script>
 
 <style scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.2s ease, opacity 0.2s ease;
+}
+
+.slide-enter-from {
+  transform: translateX(-80%);
+  opacity: 0;
+}
+
+.slide-leave-to {
+  transform: translateX(-80%);
+  opacity: 0;
+}
+
 #app {
   min-width: 100vw;
   min-height: 100vh;
